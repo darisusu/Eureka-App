40789922c880f1275b21e8ae8b103eeec48cde8c
Darius Deng <dariusdengyang@gmail.com>
2026-01-10 17:41:12 +0800

    Update components and prd

--- Eureka-mobile/app/(tabs)/cart.tsx
+++ Eureka-mobile/app/(tabs)/cart.tsx
@@ -1,12 +1,86 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+// Currently fixed cart page without dynamic data
 
+import CartItem from "@/components/CartItem";
+import CustomButton from "@/components/CustomButton";
+import CustomHeader from "@/components/CustomHeader";
+import { useCartStore } from "@/store/cart.store";
+import type { PaymentInfoStripeProps } from "@/type";
+import cn from "clsx";
+import React from "react";
+import { FlatList, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const PaymentInfoStripe = ({
+  label,
+  value,
+  labelStyle,
+  valueStyle,
+}: PaymentInfoStripeProps) => (
+  <View className="flex-between flex-row my-1">
+    <Text className={cn("paragraph-medium text-gray-200", labelStyle)}>
+      {label}
+    </Text>
+    <Text className={cn("paragraph-bold text-dark-100", valueStyle)}>
+      {value}
+    </Text>
+  </View>
+);
 const Cart = () => {
-  return (
-    <View>
-      <Text>Cart</Text>
-    </View>
-  )
-}
+  const { items, getTotalItems, getTotalPrice } = useCartStore();
+  const totalItems = getTotalItems();
+  const totalPrice = getTotalPrice();
 
-export default Cart
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartItem item={item} />}
+        keyExtractor={(item) => item.id}
+        contentContainerClassName="pb28 px-5 pt-5"
+        ListHeaderComponent={() => <CustomHeader title="Your Cart" />}
+        ListEmptyComponent={() => (
+          <View className="flex-1 justify-center items-center">
+            <View className = "p-5"><Text className="text-4xl">🛒</Text></View>
+            <Text className="h3-bold text-dark-100">Your cart is empty</Text>
+            <Text className="paragraph-regular text-gray-200 text-center mt-2">
+              Looks like you haven't added{"\n"}any food to your cart yet.
+            </Text>
+          </View>
+        )}
+        ListFooterComponent={() =>
+          totalItems > 0 && (
+            <View className="gap-5">
+              <View className="mt-6 border border-gray-200 p-5 rounded-2xl">
+                <Text className="h3-bold text-dark-100 mb-5">
+                  Payment Summary
+                </Text>
+
+                <PaymentInfoStripe
+                  label={`Total Items (${totalItems})`}
+                  value={`$${totalPrice.toFixed(2)}`}
+                />
+
+                <PaymentInfoStripe label={`Delivery Fee`} value={`$5.00`} />
+                <PaymentInfoStripe
+                  label={`Discount`}
+                  value={`- $0.50`}
+                  valueStyle="!text-success"
+                />
+                <View className="border-t border-gray-300 my-2" />
+                <PaymentInfoStripe
+                  label={`Total`}
+                  value={`$${(totalPrice + 5 - 0.5).toFixed(2)}`}
+                  labelStyle="base-bold !text-dark-100"
+                  valueStyle="base-bold !text-dark-100 !text-right"
+                />
+              </View>
+              <CustomButton title="Order Now" />
+            </View>
+          )
+        }
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Cart;
